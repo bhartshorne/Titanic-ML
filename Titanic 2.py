1ca2d521ef49753df590f376a087a0bd73d9350b
Billy Hansen <bhansen@nsareit.net>
2020-05-14 10:22:21 -0600

    Grid Search for Logistic Regression and SVM

--- Titanic 2.py
+++ Titanic 2.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor,BaggingRegressor,GradientBoostingRegressor,AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
 import os
@@ -22,23 +22,22 @@ test = pd.read_csv('test.csv')
 print(train.shape)
 print(test.shape)
 
-
 # Check Target Variable distribution
-train.Survived.value_counts(normalize = True)
+train.Survived.value_counts(normalize=True)
 
 # Check Relationships to target variable
 
 # Percentage of total by gender
-train.Sex.value_counts(normalize = True)
+train.Sex.value_counts(normalize=True)
 
 # Percentage of survival by gender
-train['Survived'].groupby(train['Sex']).mean() # Looks like Sex is a very important variable
+train['Survived'].groupby(train['Sex']).mean()  # Looks like Sex is a very important variable
 
 # Percentage of total by PClass
-train.Pclass.value_counts(normalize = True)
+train.Pclass.value_counts(normalize=True)
 
 # Percentage survived by PClass
-train['Survived'].groupby(train['Pclass']).mean() # Another Useful Variable
+train['Survived'].groupby(train['Pclass']).mean()  # Another Useful Variable
 
 # Percentage of total by embarked
 train.Embarked.value_counts(normalize=True)
@@ -53,7 +52,7 @@ gned to a cabin anyway.
 Let's look at some correlation stats for our ordinal variables. 
 '''
 
-train[['Survived', 'Parch', 'SibSp']].corr(method='spearman') # Not a strong correlation.
+train[['Survived', 'Parch', 'SibSp']].corr(method='spearman')  # Not a strong correlation.
 
 '''Looks like ther are slight, positive correlations. Now for feature engineering.
 So not to repeat steps I'm going to join both datasets and then break them apart when I'm done.'''
@@ -61,7 +60,8 @@ So not to repeat steps I'm going to join both datasets and then break them apart
 df = train.append(test, sort=True)
 
 # Drop Passenger ID
-df = df.drop(columns = ['PassengerId'])
+df = df.drop(columns=['PassengerId'])
+
 
 # Function to split and re-group name column
 def name_func(row):
@@ -69,9 +69,9 @@ def name_func(row):
         return 'Mr.'
     elif row['Name'].split(' ')[1] == 'Mrs.':
         return 'Mrs.'
-    elif row['Name'].split(' ')[1] ==  'Miss.':
+    elif row['Name'].split(' ')[1] == 'Miss.':
         return 'Miss.'
-    elif row['Name'].split(' ')[1] ==  'Dr.':
+    elif row['Name'].split(' ')[1] == 'Dr.':
         return 'Dr.'
     else:
         return 'Other'
@@ -82,10 +82,10 @@ df['Name Split'] = df.apply(name_func, axis=1)
 # Look at Name Split stats
 df['Name Split'].value_counts(normalize=True)
 
-df['Survived'].groupby(df['Name Split']).mean() # looks interested but maybe too correlated with Sex variable?
+df['Survived'].groupby(df['Name Split']).mean()  # looks interested but maybe too correlated with Sex variable?
 
 # Drop Name Column
-df = df.drop(columns = ['Name'])
+df = df.drop(columns=['Name'])
 
 # Check for Nulls
 df.isnull().sum()
@@ -107,23 +107,23 @@ df.Age.isnull().sum()
 # # # # Let's fill age NAs with a ML model
 
 # Drop irrelevant columns for this model - could redo this once I have useful data for cabin and ticket
-age = df.drop(columns = ['Cabin', 'Survived', 'Ticket'])
+age = df.drop(columns=['Cabin', 'Survived', 'Ticket'])
 
 # Pclass should be a categorical variable
 age['Pclass'] = age['Pclass'].astype('object')
 
 # Create Dummies
-age = pd.get_dummies(age, columns = ['Name Split', 'Sex', 'Pclass', 'Embarked'], drop_first = True)
+age = pd.get_dummies(age, columns=['Name Split', 'Sex', 'Pclass', 'Embarked'], drop_first=True)
 
 # Create train and predict datasets
 age_train = age[age['Age'].notnull()]
 age_test = age[age['Age'].isnull()]
-age_test = age_test.drop(columns = ['Age'])
+age_test = age_test.drop(columns=['Age'])
 len(age_train)
 len(age_test)
 
 # Grab target vector
-age_X = age_train.drop(columns = ['Age'])
+age_X = age_train.drop(columns=['Age'])
 age_y = age_train['Age']
 
 # Create Random Forest model
@@ -134,7 +134,6 @@ scores = cross_val_score(rf, age_X, age_y, cv=5, scoring='neg_mean_squared_error
 
 # Mean score with 95% confidence interval
 print("RF MSE: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
 
 # Create Adaboost Model
 ad = AdaBoostRegressor(n_estimators=100, learning_rate=1)
@@ -160,9 +159,9 @@ df.Cabin.isnull().sum() / len(df)
 '''Because 77% of the values are null, I'm going to drop this column.
 I'd be interested to see how others filled this column effectively, and if it had value after doing so.'''
 
-df = df.drop(columns = ['Cabin'])
+df = df.drop(columns=['Cabin'])
 
-df.isnull().sum() # Nulls are all clean.
+df.isnull().sum()  # Nulls are all clean.
 
 '''# I'm also going to drop the ticket number column for now. I'm don't have
 a clear idea how this column could be engineered into something useful.
@@ -177,12 +176,11 @@ train = df_ready[~pd.isnull(df_ready['Survived'])]
 test = df_ready[pd.isnull(df_ready['Survived'])].drop(columns=['Survived'])
 
 # Grab Target
-X = train.drop(columns = ['Survived'])
+X = train.drop(columns=['Survived'])
 y = train['Survived']
 
 '''For some models we'll want to use feature scaling, and for our
 tree based models we won't want to. I'll created scaled, and unscaled versions of the data.'''
-
 
 sc = StandardScaler()
 Xs = sc.fit_transform(X)
@@ -202,10 +200,49 @@ svm = SVC(gamma='scale')
 scores = cross_val_score(svm, Xs, y, cv=10, scoring='accuracy')
 print("Simple Support Vector Machine Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
+# Grid Search for Logistic Regression - We'll split the data for this step
+X_train_S, X_test_S, y_train_S, y_test_S = train_test_split(Xs, y, test_size=.15, random_state=12)
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+
+lr = LogisticRegression(solver='liblinear')
+params = {'C': [.5, 1, 1.5], 'max_iter': [100, 50, 150]}
+
+grid_lr = GridSearchCV(lr,
+                       params,
+                       scoring='accuracy',
+                       cv=10)
+
+grid_lr.fit(X_train_S, y_train_S)
+
+preds = grid_lr.predict(X_test_S)
+
+print(grid_lr.best_params_)
+print("Accuracy For Logistic Regression: ", round(accuracy_score(y_test_S, preds), 2))
+
+# Grid Search for SVM
+svm = SVC(gamma='scale')
+params = {'C': [.5, 1, 1.5, 2], 'kernel': ['rbf', 'linear']}
+
+grid_svm = GridSearchCV(svm,
+                       params,
+                       scoring='accuracy',
+                       cv=10)
+
+grid_svm.fit(X_train_S, y_train_S)
+
+preds = grid_svm.predict(X_test_S)
+
+print(grid_svm.best_params_)
+print("Accuracy For SVM: ", round(accuracy_score(y_test_S, preds), 2))
+
+
 '''Next steps:
-- Tune parameters for both these models
 - Train tree based models (XGBoost, AdaBoost, Random Forest)
 - Tune those models
 - Make first submission
 - Add feature engineering for dropped columns above.
 '''
+
+#
